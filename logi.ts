11c84e2cf41dd04958839ec86161a7845c9a9db5
Farhat Naz <159726939+Farhat-Naz@users.noreply.github.com>
2024-04-23 01:47:25 +0500

    Add files via upload

---
+++ logi.ts
@@ -0,0 +1,8 @@
+//logical operators
+let num1: number=14;
+let num2: number=5;
+console.log(num1>num2);
+console.log( 14>5 && 5<14);
+console.log(14>5 || 5>14);
+console.log(!(14>5));
+console.log(14>=5);
